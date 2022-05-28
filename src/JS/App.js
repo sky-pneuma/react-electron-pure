525c9bcdfe33bb2ec96e8afa16f25a5c11916304
Viacheslav <v.senkecich@intelico.su>
2022-05-28 21:56:40 +0300

    first commit

---
+++ src/JS/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../scss/main.css';
+
+const App = () =>  {
+  return (
+    <div className="app-wrapper">
+     <span>React Electron App</span>
+    </div>
+  );
+}
+
+export default App;
